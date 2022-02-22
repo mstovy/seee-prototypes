eb1e54df447d830b36881cc63106066034b97d3d
mstovy <micahstovall@Micahs-Macbook-Pro-2.local>
2022-02-22 15:33:09 -0700

    Text Entering Works

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 function remove(el) {
     var element = el;
     var title = $(element).attr('title');
-    console.log(title);
     $('.mutliSelect input[value="' + title + '"]').prop("checked", false);
     element.remove();
+}
+
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("multiSelect");
+    console.log(div);
+    a = div.getElementsByTagName('li');
+    console.log(a);
+    for (i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
 }
